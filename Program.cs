042af8f57c3badc0cfb4ac0fff268feeed786930
diego-lanca <diegolanca.o@gmail.com>
2023-12-29 17:55:25 -0300

    final functions, menu finished

--- Program.cs
+++ Program.cs
@@ -1,30 +1,42 @@
 ﻿using KokiBooks.Services.DataAccess;
 
 // // See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Inicializando serviço...");
+Console.Clear();
+Console.WriteLine("Inicializando serviço...");
 var books = new BookRepository();
 
-books.selectAllBooks();
-books.lendBook();
-books.selectAllBooks();
+// books.selectAllBooks();
+// books.lendBook();
+// books.selectAllBooks();
 
-// bool menu = true;
+bool menu = true;
 
-// while (menu) {
-//     Console.WriteLine("Seja bem-vindo a KokiBooks!\nSelecione a opção que deseja: ");
-//     Console.WriteLine("(1) - Verificar livros\n(2) - Empréstimo de Livro\n(3) - Devolução de Livro\n(4) - Sair");
-//     var resposta = Console.ReadLine();
+while (menu) {
+    Console.WriteLine("Seja bem-vindo a KokiBooks!\nSelecione a opção que deseja: ");
+    Console.WriteLine("(1) - Verificar livros\n(2) - Empréstimo de Livro\n(3) - Devolução de Livro\n(4) - Sair");
+    var resposta = Console.ReadLine();
 
-//     switch (resposta) {
-//         case "4":
-//             menu = false;
-//             break;
-//         default:
-//             break;
-//     }
+    Console.Clear();
+    switch (resposta) {
+        case "1":
+            books.selectAllBooks();
+            break;
+        case "2":
+            books.lendBook();
+            break;
+        case "3":
+            books.borrowBook();
+            break;
+        case "4":
+            menu = false;
+            break;
+        default:
+            break;
+    }
 
-// }
+    Console.WriteLine("\nAperte qualquer botão para prosseguir...");
+    Console.ReadLine();
+    Console.Clear();
 
+}
 
-// Console.WriteLine("Aperte qualquer botão para prosseguir...");
-// Console.ReadLine();
--- src/KokiBooks/Services/DataAccess/BookRepository.cs
+++ src/KokiBooks/Services/DataAccess/BookRepository.cs
@@ -6,19 +6,23 @@ namespace KokiBooks.Services.DataAccess
 {
     public class BookRepository
     {
-        public void selectAllBooks() {
-            using (var context = new DataService()) {
-                
+        public void selectAllBooks()
+        {
+            using (var context = new DataService())
+            {
+
                 // Define a variável livro e inclui as outras tabelas
                 var livros = context.book.Include(b => b.autor).Include(b => b.editora);
 
                 // Realiza a query, selecionando tudo de livros já com as outras tabelas
-                var query = livros.Select(l => new 
-                { Id = l.id, 
-                Nome = l.nome, 
-                Autor = l.autor != null ? l.autor.nome : null, 
-                Editora = l.editora != null ? l.editora.nome : null,
-                Estado = l.emprestado == false ? "Disponível" : "Emprestado" });
+                var query = livros.Select(l => new
+                {
+                    Id = l.id,
+                    Nome = l.nome,
+                    Autor = l.autor != null ? l.autor.nome : null,
+                    Editora = l.editora != null ? l.editora.nome : null,
+                    Estado = l.emprestado == false ? "Disponível" : "Emprestado"
+                });
 
                 // Transforma os resultados em um Frame do Deedle
                 var frame = Frame.FromRecords(query);
@@ -29,27 +33,100 @@ namespace KokiBooks.Services.DataAccess
             }
         }
 
-        public void lendBook() {
-            using (var context = new DataService()) {
+        public void lendBook()
+        {
+            using (var context = new DataService())
+            {
 
                 int idLivro = chooseBook();
 
                 var livroToUpdate = context.book.FirstOrDefault(b => b.id == idLivro);
 
-                Console.WriteLine($"--- Livro Encontrado ---\nId: {livroToUpdate?.id} | Nome: {livroToUpdate?.nome}");
+                Console.WriteLine($"\n--- Livro Encontrado ---\nId: {livroToUpdate?.id} | Nome: {livroToUpdate?.nome}");
 
-                if (livroToUpdate != null) {
+                Console.WriteLine("\nTem certeza que deseja emprestar este livro?\nDigite (S) ou (N): ");
 
-                    bool confirmacao;
-                    
-                    livroToUpdate.emprestado = true;
+                string confirmacao;
+                bool continuar = false;
 
-                    context.SaveChanges();
+                do
+                {
+                    confirmacao = Console.ReadLine();
+
+                    switch (confirmacao)
+                    {
+                        case "s" or "S":
+                            continuar = true;
+                            break;
+                        case "n" or "N":
+                            continuar = false;
+                            break;
+                        default:
+                            Console.WriteLine("Comando não reconhecido, digite novamente: ");
+                            break;
+                    }
+                } while (!(confirmacao.ToLower() == "s" || confirmacao.ToLower() == "n"));
+
+                if (livroToUpdate != null)
+                {
+                    if (continuar == true) {
+
+                        livroToUpdate.emprestado = true;
+
+                        context.SaveChanges();
+                    }
                 }
             }
         }
 
-        private int chooseBook() {
+        public void borrowBook()
+        {
+            using (var context = new DataService())
+            {
+
+                int idLivro = chooseBook();
+
+                var livroToUpdate = context.book.FirstOrDefault(b => b.id == idLivro);
+
+                Console.WriteLine($"\n--- Livro Encontrado ---\nId: {livroToUpdate?.id} | Nome: {livroToUpdate?.nome}");
+
+                Console.WriteLine("\nTem certeza que esse livro foi devolvido?\nDigite (S) ou (N): ");
+
+                string confirmacao;
+                bool continuar = false;
+
+                do
+                {
+                    confirmacao = Console.ReadLine();
+
+                    switch (confirmacao)
+                    {
+                        case "s" or "S":
+                            continuar = true;
+                            break;
+                        case "n" or "N":
+                            continuar = false;
+                            break;
+                        default:
+                            Console.WriteLine("Comando não reconhecido, digite novamente: ");
+                            break;
+                    }
+                } while (!(confirmacao.ToLower() == "s" || confirmacao.ToLower() == "n"));
+
+                if (livroToUpdate != null)
+                {
+                    if (continuar == true) {
+
+                        livroToUpdate.emprestado = false;
+
+                        context.SaveChanges();
+                    }
+                }
+            }
+        }
+
+        private int chooseBook()
+        {
             Console.WriteLine("Digite o ID do Livro que deseja selecionar: ");
             return Int32.Parse(Console.ReadLine());
         }
